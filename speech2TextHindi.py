995ef1715d8726bdc9068bd3790479444b8b1fc2
siddu1998 <samaram@ucsc.edu>
2022-06-19 00:15:51 -0700

    reading dataset issue

---
+++ speech2TextHindi.py
@@ -0,0 +1,110 @@
+import torch
+import torchaudio
+from datasets import load_dataset
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+import argparse
+import tempfile
+import queue
+import sys
+
+import sounddevice as sd
+import soundfile as sf
+import numpy  # Make sure NumPy is loaded before it is used in the callback
+assert numpy  # avoid "imported but unused" message (W0611)
+
+
+def int_or_str(text):
+    """Helper function for argument parsing."""
+    try:
+        return int(text)
+    except ValueError:
+        return text
+
+
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument(
+    '-l', '--list-devices', action='store_true',
+    help='show list of audio devices and exit')
+args, remaining = parser.parse_known_args()
+if args.list_devices:
+    print(sd.query_devices())
+    parser.exit(0)
+parser = argparse.ArgumentParser(
+    description=__doc__,
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    parents=[parser])
+parser.add_argument(
+    'filename', nargs='?', metavar='FILENAME',
+    help='audio file to store recording to')
+parser.add_argument(
+    '-d', '--device', type=int_or_str,
+    help='input device (numeric ID or substring)')
+parser.add_argument(
+    '-r', '--samplerate', type=int, help='sampling rate')
+parser.add_argument(
+    '-c', '--channels', type=int, default=1, help='number of input channels')
+parser.add_argument(
+    '-t', '--subtype', type=str, help='sound file subtype (e.g. "PCM_24")')
+args = parser.parse_args(remaining)
+
+q = queue.Queue()
+
+
+def callback(indata, frames, time, status):
+    """This is called (from a separate thread) for each audio block."""
+    if status:
+        print(status, file=sys.stderr)
+    q.put(indata.copy())
+
+def record_player_audio(): 
+    try:
+        if args.samplerate is None:
+            device_info = sd.query_devices(args.device, 'input')
+            # soundfile expects an int, sounddevice provides a float:
+            args.samplerate = int(device_info['default_samplerate'])
+        if args.filename is None:
+            args.filename = tempfile.mktemp(prefix='player_audio',
+                                            suffix='.wav', dir='player_recordings')
+
+        # Make sure the file is opened before recording anything:
+        with sf.SoundFile(args.filename, mode='x', samplerate=args.samplerate,
+                        channels=args.channels, subtype=args.subtype) as file:
+            with sd.InputStream(samplerate=args.samplerate, device=args.device,
+                                channels=args.channels, callback=callback):
+                print('#' * 80)
+                print('press Ctrl+C to stop the recording')
+                print('#' * 80)
+                while True:
+                    file.write(q.get())
+    except KeyboardInterrupt:
+        print('\nRecording finished: ' + repr(args.filename))
+        #parser.exit(0)
+    except Exception as e:
+      print('Some Error in Audio recoding')
+      #parser.exit(type(e).__name__ + ': ' + str(e))
+
+record_player_audio()
+
+test_dataset = load_dataset("player_recordings")
+processor = Wav2Vec2Processor.from_pretrained("theainerd/Wav2Vec2-large-xlsr-hindi")
+model = Wav2Vec2ForCTC.from_pretrained("theainerd/Wav2Vec2-large-xlsr-hindi")
+resampler = torchaudio.transforms.Resample(48_000, 16_000)
+
+# Preprocessing the datasets.
+# We need to read the aduio files as arrays
+def speech_file_to_array_fn(batch):
+  speech_array, sampling_rate = torchaudio.load(batch["path"])
+  batch["speech"] = resampler(speech_array).squeeze().numpy()
+  return batch
+
+test_dataset = test_dataset.map(speech_file_to_array_fn)
+inputs = processor(test_dataset["speech"][:2], sampling_rate=16_000, return_tensors="pt", padding=True)
+
+with torch.no_grad():
+  logits = model(inputs.input_values, attention_mask=inputs.attention_mask).logits
+
+predicted_ids = torch.argmax(logits, dim=-1)
+
+print("Prediction:", processor.batch_decode(predicted_ids))
+print("Reference:", test_dataset["sentence"][:2])
+
