927640bb643b1c1aee8b80d77b11910cbd5f5f8d
siddu1998 <samaram@ucsc.edu>
2022-06-26 19:50:06 -0700

    music

--- main.py
+++ main.py
@@ -50,9 +50,9 @@ grenade_thrown = False
 
 
 #load music and sounds
-#pygame.mixer.music.load('audio/music2.mp3')
-#pygame.mixer.music.set_volume(0.3)
-#pygame.mixer.music.play(-1, 0.0, 5000)
+pygame.mixer.music.load('audio/background.mp3')
+pygame.mixer.music.set_volume(0.3)
+pygame.mixer.music.play(-1, 0.0, 5000)
 jump_fx = pygame.mixer.Sound('audio/jump.wav')
 jump_fx.set_volume(0.05)
 shot_fx = pygame.mixer.Sound('audio/shot.wav')
@@ -69,8 +69,8 @@ restart_img = pygame.image.load('img/restart_btn.png').convert_alpha()
 #background
 pine1_img = pygame.image.load('img/Background/pine1.png').convert_alpha()
 pine2_img = pygame.image.load('img/Background/pine2.png').convert_alpha()
-mountain_img = pygame.image.load('img/Background/mountain.png').convert_alpha()
-sky_img = pygame.image.load('img/Background/sky_cloud.png').convert_alpha()
+mountain_img = pygame.image.load('img/Background/cosmic.jpeg').convert_alpha()
+sky_img = pygame.image.load('img/Background/cosmic.jpeg').convert_alpha()
 #store tiles in a list
 img_list = []
 for x in range(TILE_TYPES):
@@ -93,7 +93,7 @@ item_boxes = {
 
 
 #define colours
-BG = (144, 201, 120)
+BG = (242, 214, 138)
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
@@ -113,7 +113,7 @@ def draw_bg():
 	width = sky_img.get_width()
 	for x in range(5):
 		screen.blit(sky_img, ((x * width) - bg_scroll * 0.5, 0))
-		screen.blit(mountain_img, ((x * width) - bg_scroll * 0.6, SCREEN_HEIGHT - mountain_img.get_height() - 300))
+		screen.blit(mountain_img, ((x * width) - bg_scroll * 0.6, SCREEN_HEIGHT - mountain_img.get_height()-300))
 		screen.blit(pine1_img, ((x * width) - bg_scroll * 0.7, SCREEN_HEIGHT - pine1_img.get_height() - 150))
 		screen.blit(pine2_img, ((x * width) - bg_scroll * 0.8, SCREEN_HEIGHT - pine2_img.get_height()))
 
@@ -223,7 +223,9 @@ class Soldier(pygame.sprite.Sprite):
 			self.jump = False
 			self.in_air = True
 
-		#apply gravity
+
+
+		#apply gravity always
 		self.vel_y += GRAVITY
 		if self.vel_y > 10:
 			self.vel_y
@@ -282,7 +284,6 @@ class Soldier(pygame.sprite.Sprite):
 				screen_scroll = -dx
 
 		return screen_scroll, level_complete
-
 
 
 	def shoot(self):
